3332cccfcc454be613faa228f7c1db67b17003f6
SimoneZampieri <simonezampieri106@gmail.com>
2025-01-28 16:48:22 +0100

    [update] header fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,9 +12,9 @@ const Header = () => {
 
   return (
     <header>
-      <nav className="navbar bg-body-tertiary">
+      <nav className="navbar bg-dark text-bg-dark">
         <div className="container-fluid">
-          <a className="navbar-brand">BoolFlix</a>
+          <a className="navbar-brand font-weight-bold text-danger">BoolFlix</a>
           <form className="d-flex" role="search" onSubmit={searchHand}>
             <input
               className="form-control me-2"
--- src/components/ShowCard.jsx
+++ src/components/ShowCard.jsx
@@ -5,7 +5,7 @@ const ShowCards = ({ media }) => {
       <div className="card">
         <img className="img-fluid" src="" alt="" />
         <div className="card-body">
-          <h5 className="card-text">Titolo: {media.title}</h5>
+          <h5 className="card-text">Titolo: {media.original_name}</h5>
           <p className="card-text">Titolo originale: {media.original_title}</p>
           <p>Lingua: {media.original_language}</p>
           <p>Voto: {rating}</p>
