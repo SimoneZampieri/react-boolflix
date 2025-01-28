ff921b5dfba5612c00f0a67c10f0cc36a04dfc7b
SimoneZampieri <simonezampieri106@gmail.com>
2025-01-28 11:35:44 +0100

    [update] defined context

--- src/context/GlobalContext.jsx
+++ src/context/GlobalContext.jsx
@@ -6,7 +6,18 @@ const GlobalContext = createContext();
 const apiKey = import.meta.env.VITE_API_KEY;
 
 function GlobalProvider({ children }) {
-  return <GlobalContext.Provider>{children}</GlobalContext.Provider>;
+  const [movies, setMovies] = useState([]);
+
+  const fetchMovies = () => {
+    axios.get(apiKey).then((res) => {
+      setMovies(res.data);
+    });
+  };
+  return (
+    <GlobalContext.Provider value={{ movies, setMovies, fetchMovies }}>
+      {children}
+    </GlobalContext.Provider>
+  );
 }
 
 const useGlobalContext = () => {
