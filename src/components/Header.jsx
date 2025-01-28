6fc203ea192037f13ae9505ea5a7871804b634f2
SimoneZampieri <simonezampieri106@gmail.com>
2025-01-28 11:09:15 +0100

    [update] app.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,24 @@
 const Header = () => {
-    return (
-        <header>
-            <nav class="navbar bg-body-tertiary">
-                <div class="container-fluid">
-                    <a class="navbar-brand">Navbar</a>
-                    <form class="d-flex" role="search">
-                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                        <button class="btn btn-outline-success" type="submit">Search</button>
-                    </form>
-                </div>
-            </nav>
-        </header>
-    );
+  return (
+    <header>
+      <nav className="navbar bg-body-tertiary">
+        <div className="container-fluid">
+          <a className="navbar-brand">Navbar</a>
+          <form className="d-flex" role="search">
+            <input
+              className="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button className="btn btn-outline-success" type="submit">
+              Search
+            </button>
+          </form>
+        </div>
+      </nav>
+    </header>
+  );
 };
 
-export default Header
+export default Header;
