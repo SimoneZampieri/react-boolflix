8329794828d6ed1c498f74b674c2e1377bc87989
SimoneZampieri <simonezampieri106@gmail.com>
2025-01-28 12:06:45 +0100

    [update] major header improvements

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,28 @@
+import { useState } from "react";
+import { useGlobalContext } from "../context/GlobalContext";
+
 const Header = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const { fetchMovies } = useGlobalContext();
+
+  const searchHand = (e) => {
+    e.preventDefault();
+    fetchMovies(searchTerm);
+  };
+
   return (
     <header>
       <nav className="navbar bg-body-tertiary">
         <div className="container-fluid">
           <a className="navbar-brand">BoolFlix</a>
-          <form className="d-flex" role="search">
+          <form className="d-flex" role="search" onSubmit={searchHand}>
             <input
               className="form-control me-2"
               type="search"
               placeholder="Search"
               aria-label="Search"
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
             />
             <button className="btn btn-outline-success" type="submit">
               Search
