16564f8e4a698691925d5472e8822150d574a3b7
SimoneZampieri <simonezampieri106@gmail.com>
2025-01-29 16:47:18 +0100

    [update] major css updates

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,7 +1,9 @@
 import RatingSys from "./RatingSys";
+import { useState } from "react";
 
 const MediaCards = ({ media }) => {
   const rating = Math.ceil(media.vote_average / 2);
+  const [isHovered, setIsHovered] = useState(false);
 
   const flags = {
     it: "lang-flags/italian.png",
@@ -10,22 +12,32 @@ const MediaCards = ({ media }) => {
 
   return (
     <div className="col-4 p-3">
-      <div className="card">
+      <div
+        className="card"
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+      >
         <img
           className="img-fluid"
           src={`https://image.tmdb.org/t/p/w200${media.poster_path}`}
-          alt=""
+          alt={`Poster of ${media.title}`}
         />
         <div className="card-body">
-          <h5 className="card-text">Titolo: {media.title}</h5>
-          <p className="card-text">Titolo originale: {media.original_title}</p>
-          <div className="imgcont">
-            <img
-              src={flags[media.original_language]}
-              alt={media.original_language}
-            />
-          </div>
-          <RatingSys vote={rating} />
+          {isHovered && (
+            <>
+              <h5 className="card-text">Titolo: {media.title}</h5>
+              <p className="card-text">
+                Titolo originale: {media.original_title}
+              </p>
+              <div className="imgcont">
+                <img
+                  src={flags[media.original_language]}
+                  alt={media.original_language}
+                />
+              </div>
+              <RatingSys vote={rating} />
+            </>
+          )}
         </div>
       </div>
     </div>
