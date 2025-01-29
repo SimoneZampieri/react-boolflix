1dcab315abfb015d992dd8a5694977c3083df60a
SimoneZampieri <simonezampieri106@gmail.com>
2025-01-29 12:38:18 +0100

    [update] new logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -15,7 +15,7 @@ const Header = () => {
     <header>
       <nav className="navbar bg-dark text-bg-dark">
         <div className="container-fluid">
-          <h1 className="font-weight-bold text-danger">BoolFlix</h1>
+          <img src="./assets/logo.png" alt="BoolFlix" />
           <form className="d-flex" role="search" onSubmit={searchHand}>
             <input
               className="form-control me-2"
